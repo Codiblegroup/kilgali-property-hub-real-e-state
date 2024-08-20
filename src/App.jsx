d3985d42abc841c6c957d80368148ed884186548
developer <developer@gmail.com>
2024-08-20 16:08:14 +0200

    ft add product list

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ProductContainer from './components/products/ProductContainer'
+
+const App = () => {
+  return (
+    <div className='  px-[50px] md:px-[100px] lg:px-[150px] py-[50px]'>
+         <ProductContainer />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/data/procard.js
@@ -0,0 +1,63 @@
+import pro1 from "/pro-1.png";
+import pro2 from "/pro-2.png";
+import pro3 from "/pro-3.png";
+import pro4 from "/pro-4.png";
+import pro5 from "/pro-5.png";
+import pro6 from "/pro-6.png";
+
+export const procardDatas = [
+  {
+    image: `.${pro1}`,
+    title: "Luxury Family Home",
+    location: "1800-1818 79th St",
+    price: 400,
+    room: 4,
+    washroom: 2,
+    outer: 400,
+  },
+  {
+    image: `.${pro2}`,
+    title: "Luxury Family Home",
+    location: "1800-1818 79th St",
+    price: 400,
+    room: 4,
+    washroom: 2,
+    outer: 400,
+  },
+  {
+    image: `.${pro3}`,
+    title: "Luxury Family Home",
+    location: "1800-1818 79th St",
+    price: 400,
+    room: 4,
+    washroom: 2,
+    outer: 400,
+  },
+  {
+    image: `.${pro4}`,
+    title: "Luxury Family Home",
+    location: "1800-1818 79th St",
+    price: 400,
+    room: 4,
+    washroom: 2,
+    outer: 400,
+  },
+  {
+    image: `.${pro5}`,
+    title: "Luxury Family Home",
+    location: "1800-1818 79th St",
+    price: 400,
+    room: 4,
+    washroom: 2,
+    outer: 400,
+  },
+  {
+    image: `.${pro6}`,
+    title: "Luxury Family Home",
+    location: "1800-1818 79th St",
+    price: 400,
+    room: 4,
+    washroom: 2,
+    outer: 400,
+  },
+];
